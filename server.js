387cf194478b6eceac3484b3106befd56f4dcff5
hanielchang <haniellloyd@gmail.com>
2021-09-05 01:02:03 -0700

    finished front end

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 // Function to filter query
 function filterByQuery(query, animalsArray) {
@@ -101,6 +102,22 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // Route to receive data from client
